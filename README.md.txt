84d5e8c847e4442a58a85fee3e768de2305cac2c
ferrwanz <ferrwanz@gmail.com>
2016-09-28 01:50:35 +0700

    first commit & create practive_1_1

---
+++ README.md.txt
@@ -0,0 +1,3 @@
+# Ferry Irawan - 141110507
+
+This repo is use for tracking my progress in "Pemograman Framework .NET Lanjutan" lecture that will be assigned to [Sir. Denny Sutomo] (https://github.com/dennysu93).
---
+++ Senin_141110507_FerryIrawan/Latihan_1_1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_1
+{
+    public partial class Form1 : Form
+    {
+        int nowYear = DateTime.Today.Year;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.label1.Text = Convert.ToString(this.vScrollBar1.Value);
+            this.label2.Text = Convert.ToString(this.vScrollBar2.Value);
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.changeMin);
+            this.vScrollBar2.ValueChanged += new System.EventHandler(this.changeMax);
+            this.dateTimePicker1.MinDate = new DateTime(nowYear - this.vScrollBar1.Value, 1, 1);
+            this.dateTimePicker1.MaxDate = new DateTime(nowYear - this.vScrollBar2.Value, 12, 31);
+        }
+        private void changeMin(object sender, EventArgs e)
+        {
+            this.label1.Text = Convert.ToString(this.vScrollBar1.Value);
+            this.dateTimePicker1.MinDate = new DateTime(nowYear - this.vScrollBar1.Value, 1, 1);
+        }
+        private void changeMax(object sender, EventArgs e)
+        {
+            this.label2.Text = Convert.ToString(this.vScrollBar2.Value);
+            this.dateTimePicker1.MaxDate = new DateTime(nowYear - this.vScrollBar2.Value, 12, 31);
+        }
+    }
+}
