cfb12bede3a96066d3375bc8585815b3f577110e
ferrwanz <ferrwanz@gmail.com>
2016-10-05 23:48:24 +0700

    Add Latihan_2_1 with Material Design

---
+++ Latihan_2_1/Form2_1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form2_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.daysNumber = new System.Windows.Forms.DomainUpDown();
+            this.monthsName = new System.Windows.Forms.DomainUpDown();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.infoText = new MaterialSkin.Controls.MaterialLabel();
+            this.yearPicker = new System.Windows.Forms.DateTimePicker();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.deletedInfo = new MaterialSkin.Controls.MaterialLabel();
+            this.deleteButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // daysNumber
+            // 
+            this.daysNumber.Location = new System.Drawing.Point(208, 42);
+            this.daysNumber.Name = "daysNumber";
+            this.daysNumber.Size = new System.Drawing.Size(48, 20);
+            this.daysNumber.TabIndex = 0;
+            // 
+            // monthsName
+            // 
+            this.monthsName.Location = new System.Drawing.Point(281, 42);
+            this.monthsName.Name = "monthsName";
+            this.monthsName.Size = new System.Drawing.Size(89, 20);
+            this.monthsName.TabIndex = 0;
+            this.monthsName.SelectedItemChanged += new System.EventHandler(this.monthsName_SelectedItemChanged);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.monthCalendar1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthCalendar1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.monthCalendar1.Location = new System.Drawing.Point(26, 11);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            this.monthCalendar1.TitleBackColor = System.Drawing.Color.Brown;
+            this.monthCalendar1.TitleForeColor = System.Drawing.Color.AliceBlue;
+            this.monthCalendar1.TrailingForeColor = System.Drawing.SystemColors.HotTrack;
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(-1, 97);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(757, 395);
+            this.materialTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.materialTabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.infoText);
+            this.tabPage1.Controls.Add(this.yearPicker);
+            this.tabPage1.Controls.Add(this.materialLabel3);
+            this.tabPage1.Controls.Add(this.materialLabel2);
+            this.tabPage1.Controls.Add(this.materialLabel1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.addButton);
+            this.tabPage1.Controls.Add(this.daysNumber);
+            this.tabPage1.Controls.Add(this.monthsName);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(749, 369);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Add Special Day";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // infoText
+            // 
+            this.infoText.AutoSize = true;
+            this.infoText.Depth = 0;
+            this.infoText.Font = new System.Drawing.Font("Roboto", 11F);
+            this.infoText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.infoText.Location = new System.Drawing.Point(186, 74);
+            this.infoText.MinimumSize = new System.Drawing.Size(370, 0);
+            this.infoText.MouseState = MaterialSkin.MouseState.HOVER;
+            this.infoText.Name = "infoText";
+            this.infoText.Size = new System.Drawing.Size(370, 19);
+            this.infoText.TabIndex = 5;
+            this.infoText.Text = "materialLabel4";
+            this.infoText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yearPicker
+            // 
+            this.yearPicker.Location = new System.Drawing.Point(390, 42);
+            this.yearPicker.Name = "yearPicker";
+            this.yearPicker.Size = new System.Drawing.Size(91, 20);
+            this.yearPicker.TabIndex = 4;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(386, 20);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(39, 19);
+            this.materialLabel3.TabIndex = 3;
+            this.materialLabel3.Text = "Year";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(278, 20);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(52, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Month";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(204, 20);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(34, 19);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "Day";
+            this.materialLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(192, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Depth = 0;
+            this.addButton.Location = new System.Drawing.Point(487, 38);
+            this.addButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.addButton.Name = "addButton";
+            this.addButton.Primary = true;
+            this.addButton.Size = new System.Drawing.Size(59, 28);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.deletedInfo);
+            this.tabPage2.Controls.Add(this.monthCalendar1);
+            this.tabPage2.Controls.Add(this.deleteButton);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(749, 369);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "View Calendar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // deletedInfo
+            // 
+            this.deletedInfo.AutoSize = true;
+            this.deletedInfo.Depth = 0;
+            this.deletedInfo.Font = new System.Drawing.Font("Roboto", 11F);
+            this.deletedInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.deletedInfo.Location = new System.Drawing.Point(133, 329);
+            this.deletedInfo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.deletedInfo.Name = "deletedInfo";
+            this.deletedInfo.Size = new System.Drawing.Size(108, 19);
+            this.deletedInfo.TabIndex = 4;
+            this.deletedInfo.Text = "materialLabel4";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Depth = 0;
+            this.deleteButton.Location = new System.Drawing.Point(31, 321);
+            this.deleteButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Primary = true;
+            this.deleteButton.Size = new System.Drawing.Size(75, 34);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(-1, 64);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(1368, 41);
+            this.materialTabSelector1.TabIndex = 5;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form2_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 497);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Controls.Add(this.materialTabControl1);
+            this.Name = "Form2_1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan_2_1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown daysNumber;
+        private System.Windows.Forms.DomainUpDown monthsName;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private MaterialSkin.Controls.MaterialRaisedButton deleteButton;
+        private MaterialSkin.Controls.MaterialRaisedButton addButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.DateTimePicker yearPicker;
+        private System.Windows.Forms.Timer timer1;
+        private MaterialSkin.Controls.MaterialLabel infoText;
+        private MaterialSkin.Controls.MaterialLabel deletedInfo;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Latihan_2_1/Form2_1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Latihan_2_1
+{
+    public partial class Form2_1 : MaterialForm
+    {
+        public Form2_1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightBlue900, Primary.BlueGrey900, Primary.Green600, Accent.LightBlue200, TextShade.WHITE);
+            infoText.Text = deletedInfo.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime birth = new DateTime(1997, 02, 07),
+                startDate = new DateTime(2016, 01, 01);
+            TimeSpan diff = new DateTime(2016, 12, 31) - startDate;
+
+            for(var i=0;i<=diff.Days;i++)
+            {
+                var testDate = startDate.AddDays(i);
+                switch(testDate.DayOfWeek)
+                {
+                    case DayOfWeek.Saturday:
+                    case DayOfWeek.Sunday:
+                        DateTime weekday = new DateTime(testDate.Year, testDate.Month, testDate.Day);
+                        monthCalendar1.AddBoldedDate(weekday);
+                        break;
+                }
+            }
+
+            listBox1.Items.Add("My Birthday: " + birth.Date);
+            monthCalendar1.AddAnnuallyBoldedDate(birth);
+            yearPicker.Format = DateTimePickerFormat.Custom;
+            yearPicker.CustomFormat = "yyyy";
+            yearPicker.ShowUpDown = true;
+            string[] month = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            for (int i = 0; i < month.Length; i++)
+                monthsName.Items.Add(month[i]);
+            monthsName.SelectedIndex = 0;
+            yearPicker.TextChanged += new System.EventHandler(monthsName_SelectedItemChanged);
+
+            addButton.Click += new System.EventHandler(addButton_Click);
+            deleteButton.Click += new System.EventHandler(deleteButton_Click);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            DateTime specialDay = new DateTime(yearPicker.Value.Year, monthsName.SelectedIndex + 1, daysNumber.SelectedIndex + 1);
+            int same = listBox1.FindStringExact(specialDay.ToShortDateString());
+            if (same == ListBox.NoMatches)
+            {
+                listBox1.Items.Add(specialDay);
+                infoText.Text = specialDay + " has been marked for a special day !";
+                infoText.ForeColor = Color.Black;
+                timer1.Start();
+            }
+            else
+            {
+                infoText.Text = "THAT DATE HAS BEEN ADDED BEFORE !!!";
+                infoText.ForeColor = Color.Black;
+                timer1.Start();
+            }
+            monthCalendar1.AddAnnuallyBoldedDate(specialDay);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            monthCalendar1.RemoveAnnuallyBoldedDate(monthCalendar1.SelectionEnd);
+            int idx = listBox1.FindStringExact(monthCalendar1.SelectionEnd.ToShortDateString());
+            if(idx != ListBox.NoMatches)
+                listBox1.Items.RemoveAt(idx);
+            deletedInfo.Text = monthCalendar1.SelectionEnd.Day + "/" + monthCalendar1.SelectionEnd.Month + " has been marked as regular day.\n(Except Saturday and Sunday in 2016, they won't be remove)";
+            deletedInfo.ForeColor = Color.Black;
+            timer2.Start();
+        }
+
+        // Change Dates Range Every Time Month || Year Change
+        private void monthsName_SelectedItemChanged(object sender, EventArgs e)
+        {
+            daysNumber.Items.Clear();
+            int[] daysOfMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int days = daysOfMonths[monthsName.SelectedIndex];
+            days = (
+                (yearPicker.Value.Year % 4 == 0 && yearPicker.Value.Year % 100 != 0) || yearPicker.Value.Year % 400 == 0 && monthsName.SelectedIndex == 1) ? 
+                days + 1 : days;
+            for (int i = 0; i < days; i++)
+            {
+                daysNumber.Items.Add(i + 1);
+            }
+            if (daysNumber.SelectedIndex + 1 > days)
+                daysNumber.SelectedIndex = days - 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int fadingSpeed = (infoText.ForeColor.R >= 90) ? 15 : 5;
+            infoText.ForeColor = Color.FromArgb(infoText.ForeColor.R + fadingSpeed, infoText.ForeColor.G + fadingSpeed, infoText.ForeColor.B + fadingSpeed);
+
+            if(infoText.ForeColor.R >= this.BackColor.R)
+            {
+                timer1.Stop();
+                infoText.ForeColor = this.BackColor;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int fadingSpeed = (deletedInfo.ForeColor.R >= 90) ? 15 : 5;
+            deletedInfo.ForeColor = Color.FromArgb(deletedInfo.ForeColor.R + fadingSpeed, deletedInfo.ForeColor.G + fadingSpeed, deletedInfo.ForeColor.B + fadingSpeed);
+
+            if (deletedInfo.ForeColor.R >= this.BackColor.R)
+            {
+                timer2.Stop();
+                deletedInfo.ForeColor = this.BackColor;
+            }
+        }
+    }
+}
